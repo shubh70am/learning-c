6397a826e7eede5d21964a946aeb9f4ec3b026fa
Shubham Kumar <kumarshubhamssb4@gmail.com>
2025-07-17 15:22:58 +0530

    commit

--- Practice Questions/pq3.c
+++ Practice Questions/pq3.c
@@ -2,7 +2,7 @@
 
 int main(){
     int n;
-    printf("entre the name of variable:");
+    printf("entre the name of variable:\n");
     scanf("%d", &n);
     int cube = n * n * n;
     printf("the cube of the n is: %d\n", cube);
